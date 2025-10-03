9b2822b0e73ddf6bac840ffc3b0711463872b3ca
rofigemaladi05-student <rofigemaladi05@gmail.com>
2025-10-03 11:03:10 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                'urap-green': '#347C5E',
+                'urap-cream': '#FCF6F0',
+                'urap-spice': '#D97706',
+                'urap-dark': '#292524',
+            },
+            fontFamily: {
+                sans: ['Inter', 'sans-serif'],
+            },
+        }
+    }
+};
